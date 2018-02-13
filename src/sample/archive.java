9a27675bafbfc406f58220a1fd05ae6c0b9903b1
YanyangChen <184536632@qq.com>
2018-02-13 18:37:32 +0800

    Update archive.java

--- src/sample/archive.java
+++ src/sample/archive.java
@@ -137,6 +137,34 @@ public class Controller implements Initializable{
             getData("0700.HK", i.toString(), i2.toString());
         }
     }
+    
+     @FXML protected void getTencentStock2Excel(ActionEvent event) throws Exception {
+        String file = "0700HK.xls";
+//        POIFSFileSystem fs = new POIFSFileSystem(new FileOutputStream(file));
+
+        FileOutputStream fs = new FileOutputStream(file);
+        HSSFWorkbook wb =  new HSSFWorkbook();
+        HSSFSheet sheet = wb.createSheet("test");
+        HSSFRow row;
+        HSSFCell cell;
+
+        int gap = 3600*24*60;
+        Integer start = 1107792000;
+        Integer end = 1518019200;
+//        Integer end = 1107792000 +  3600*24*60;
+//        org.jsoup.nodes.Document doc = Jsoup.connect("https://finance.yahoo.com/quote/0700.HK/history?period1="+start.toString()+"&period2="+end.toString()+"&interval=1d&filter=history&frequency=1d").proxy(proxy).get();
+
+//        URL url = new URL("https://finance.yahoo.com/quote/0700.HK/history?period1="+start.toString()+"&period2="+end.toString()+"&interval=1d&filter=history&frequency=1d");
+
+        for(Integer i=start.intValue(); i < end.intValue(); i += gap ) {
+            Integer i2 = i + gap;
+            if(i2 + 120 > end) i2 = end;
+           counter =  getData2excel("0700.HK", i.toString(), i2.toString(),sheet, counter);
+        }
+        wb.write(fs);
+        fs.close();
+        wb.close();
+    }
 
     @FXML protected void getTencentStock2Redis(ActionEvent event) throws Exception {
         int gap = 3600*24*60;
@@ -270,4 +298,52 @@ public class Controller implements Initializable{
 //        {System.out.println(stkque.pollFirst());}
 //    }
     }
+    
+    int getData2excel(String stock, String start, String end, HSSFSheet sheet, int counter) throws Exception{
+            URL url = new URL("https://finance.yahoo.com/quote/"+stock+"/history?period1="+start.toString()+"&period2="+end.toString()+"&interval=1d&filter=history&frequency=1d");
+            Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("proxy1.edb.gov.hk", 8080)); // or whatever your proxy is
+            System.setProperty("http.proxyHost", "proxy1.edb.gov.hk");
+            System.setProperty("http.proxyPort", "8080");
+            HttpURLConnection uc = (HttpURLConnection)url.openConnection(proxy);
+
+            uc.connect();
+
+            String line = null;
+            StringBuffer tmp = new StringBuffer();
+            BufferedReader in = new BufferedReader(new InputStreamReader(uc.getInputStream()));
+            while ((line = in.readLine()) != null) {
+                tmp.append(line);
+            }
+            //---------------------------------------------------------------------------
+//        System.setProperty("http.proxyHost", "proxy1.edb.gov.hk");
+//        System.setProperty("http.proxyPort", "8080");
+            org.jsoup.nodes.Document doc = Jsoup.parse(String.valueOf(tmp));
+            Elements tbody = doc.select("tbody");
+            Elements tr = tbody.select("tr");
+
+            ArrayDeque<String> stkque = new ArrayDeque<String>();
+            for (int i =  tr.size() -1 ; i >= 0; i--)
+            {
+            System.out.print(tr.get(i).text() + "\n") ;
+                counter++;
+               HSSFRow row = sheet.createRow(counter);
+                stkque.addFirst(tr.get(i).text());
+                if(        !tr.get(i).text().substring(12).contains("-")
+                        && !tr.get(i).text().substring(12).contains("Dividend")
+                        && !tr.get(i).text().substring(12).contains("Stock Split")){
+                row.createCell(0).setCellValue(tr.get(i).text().substring(0,12));
+
+                String[] splited = tr.get(i).text().substring(12).split(" ");
+                row.createCell(1).setCellValue(splited[0]);
+                row.createCell(2).setCellValue(splited[1]);
+                row.createCell(3).setCellValue(splited[2]);
+                row.createCell(4).setCellValue(splited[3]);
+                row.createCell(5).setCellValue(splited[4]);
+                row.createCell(6).setCellValue(splited[5]);
+                row.createCell(7).setCellValue(splited[6]);}
+            }
+//
+//    }
+            return counter;
+        }
 }
